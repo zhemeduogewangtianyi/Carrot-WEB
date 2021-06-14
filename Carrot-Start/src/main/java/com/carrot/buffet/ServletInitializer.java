3d6be539e74ce58dc5f374fa056cd4fc757e4836
WTY <wtywty521@gmail.com>
2021-06-14 11:24:41 +0800

    first commit

---
+++ Carrot-Start/src/main/java/com/carrot/buffet/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.carrot.buffet;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BuffetApplication.class);
+    }
+
+}
---
+++ Carrot-Start/src/main/java/com/carrot/buffet/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.carrot.buffet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/")
+    public String index(){
+        return "/index";
+    }
+
+}
